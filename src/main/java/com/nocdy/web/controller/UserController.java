fed4de97b9ac287f6f63ca7bfc4833da58697aca
rocky <954272099@qq.com>
2021-08-26 00:29:23 +0800

    实现登录与注册 1.0.1

--- src/main/java/com/nocdy/web/controller/UserController.java
+++ src/main/java/com/nocdy/web/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.nocdy.web.controller;
 
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nocdy.web.entity.User;
 import com.nocdy.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 
 @Controller
 public class UserController {
@@ -13,9 +21,24 @@ public class UserController {
     UserService userService;
 
 
-    @RequestMapping(value = {"/","/login"})
-    public String login(){
+    @GetMapping(value = "/")
+    public String toLogin(){
         return  "index";
+    }
+
+    @PostMapping(value = "/login")
+    public @ResponseBody Object login(@RequestParam("name") String name,
+                                      @RequestParam("password") String password){
+
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper
+                .eq("user_name",name)
+                .eq("user_password",password);
+        User user=userService.getOne(queryWrapper);
+        if(user==null)
+            return "check your info";
+        else
+            return "login success";
     }
 
     @GetMapping(value = "/registry")
@@ -24,16 +47,21 @@ public class UserController {
     }
 
     @PostMapping(value = "/registry")
-    public @ResponseBody Object registry(@RequestBody User user){
+    public @ResponseBody Object registry(@RequestParam("name") String username,
+                                         @RequestParam("password") String password,
+                                         @RequestParam("confirmPassword") String confirmPassword){
 
-        if(user.getPassword()== user.getConfirmPassword()){
+        User user=new User();
+        user.setName(username);
+        user.setPassword(password);
+        user.setConfirmPassword(confirmPassword);
+        if(user.getPassword().equals(user.getConfirmPassword())){
             userService.save(user);
             return "success";
         }
         else{
-            return "error";
+            return "failed";
         }
     }
-
 
 }
